206605c10df244cb4458ff6c8bf8bcd6a6af8926
jhaowei-huang <jhaowei.huang@gmail.com>
2016-04-07 19:42:49 +0800

    add ComponentEditorPage

---
+++ src/c4lab/iot/smarthomevisualeditor/page/ComponentEditorPage.java
@@ -0,0 +1,11 @@
+package c4lab.iot.smarthomevisualeditor.page;
+
+import c4lab.iot.smarthomevisualeditor.design.ComponentEditorPageDesign;
+
+public class ComponentEditorPage extends ComponentEditorPageDesign {
+
+	public ComponentEditorPage(String name) {
+		super();
+		this.inputName.setValue(name);
+	}
+}
